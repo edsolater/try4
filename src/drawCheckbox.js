55dc2637d73b896c86b542b8d5bebf6edac316da
edsolater <edsolater@qq.com>
2019-08-24 22:21:16 +0800

    init

---
+++ src/drawCheckbox.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './drawCheckbox.css'
+
+
+/**
+ *
+ * @type {import('./drawCheckbox').draw}
+ */
+function name(hello='world') {
+  return hello
+}
+
+
+
+/**
+ *
+ *
+ * @export
+ * @param {number} [x=0]
+ * @param {number} [y=0]
+ */
+export function abc( x=0, y=0 ) { }
