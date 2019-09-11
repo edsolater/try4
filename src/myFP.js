d165b688ac2cc7ff7abd1c9e42879941fe6ca981
zhanggongying <zhanggongying@mrbs.pro>
2019-09-11 14:50:41 +0800

    history简单化

--- src/myFP.js
+++ src/myFP.js
@@ -31,49 +31,57 @@ class Functor {
   //TODO:Typescript 中如何 “有作用域地” 判定数据类型？
   constructor(...values) {
     if (values.length === 1) {
-      this.value = values[0]
+      this._value = values[0]
     } else {
-      this.value = values
+      this._value = values
     }
-    this.valueHistory = [] // 记录值的变换
-    this.processRecord = [] //记录步骤
+    this.history = [{ value: this._value, use: [] }] // 记录一切过程
   }
-  setValue(newValue) {
+  set value(newValue) {
     //TODO:应用其上的规则，依然应用在新值上
-    const oldValue = this.value
-    this.valueHistory.push(oldValue)
-    this.value = newValue
+    this.history.push({ value: newValue, use: [] })
+    this._value = newValue
     return this
   }
-
+  get value() {
+    return this._value
+  }
+  /**
+   *
+   * 克隆涵子内的值/值们，克隆默认不包含历时记录
+   * @return {this}
+   */
+  clone({ history = false } = {}) {
+    const newWrappedValue = new this.constructor(this._value)
+    if (history === true) {
+      newWrappedValue.history = this.history.slice()
+    }
+    return newWrappedValue
+  }
   /**
    * 对涵子内的值/值们应用单/多个函数
    * @param {function[]} fns
    */
-  run(...fns) {
+  use(...fns) {
     if (fns.length) {
+      // 记录传入的fns
+      const recordedProcess = this.history[this.history.length - 1].use
+      fns.forEach(fn => recordedProcess.push(fn))
+
+      //使用
       const fn = pipe(fns)
-      if (typeOf(this.value) === 'array') {
-        const newValue =[]
-        for (const element of this.value) {
+      let newValue
+      if (typeOf(this._value) === 'array') {
+        newValue = []
+        for (const element of this._value) {
           newValue.push(fn(element))
         }
-        this.setValue(newValue)
-      } else if (typeOf(this.value) === 'function') {
-        const newValue = fn(this.value)
-        this.setValue(newValue)
+      } else if (typeOf(this._value) === 'function') {
+        newValue = fn(element)
       }
-    } else {
-      return this
+      this.value = newValue
     }
-  }
-  /**
-   *
-   * 克隆涵子内的值/值们
-   * @return {this}
-   */
-  clone() {
-    return new this.constructor(this.value)
+    return this
   }
   /**
    *  只是个快捷方式
@@ -81,7 +89,7 @@ class Functor {
    * @param {function} fn
    */
   map(...fns) {
-    return this.clone().run(...fns)
+    return this.clone().use(...fns)
   }
   /**
    *
@@ -117,5 +125,5 @@ const b = new StackFunctor({ a: [1] })
 const c = b.map(({ a }) => ({ a: a.concat(2) }))
 console.log('c: ', c)
 console.log('b: ', b)
-console.log(new StackFunctor(4).run().run(n => n * n, n => n + 2))
+console.log(new StackFunctor(4).use().use(n => n * n, n => n + 2))
 console.log(StackFunctor.prototype)
